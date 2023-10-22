b1da1f8ce3f39a3921ea312ec297516e53a7dd36
wootguy <drake.rhunter@gmail.com>
2023-10-22 15:36:37 -0700

    remove project-specific flag
    added accidentally

--- include/hlsdk/dlls/util.h
+++ include/hlsdk/dlls/util.h
@@ -410,7 +410,6 @@ extern DLL_GLOBAL int                   g_Language;
 #define SF_BREAK_TRIGGER_ONLY   1// may only be broken by trigger
 #define SF_BREAK_TOUCH                  2// can be 'crashed through' by running player (plate glass)
 #define SF_BREAK_PRESSURE               4// can be broken by a player standing on it
-#define SF_BREAK_CROWBAR                256// instant break if hit with crowbar
 
 // func_pushable (it's also func_breakable, so don't collide with those flags)
 #define SF_PUSH_BREAKABLE               128
