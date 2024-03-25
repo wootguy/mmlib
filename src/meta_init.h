bc518cc70007a23e25c7d291481cc1192295f708
wootguy <drake.rhunter@gmail.com>
2024-03-25 01:31:30 -0700

    add mod API loader for hl co-op functions
    hardcoded library paths for now. Define LOAD_GAME_API in CMakeLists.txt to request the API to be loaded.

--- src/meta_init.h
+++ src/meta_init.h
@@ -3,6 +3,7 @@
 // functions to making porting from angelscript to metamod easier
 #include <extdll.h>
 #include <meta_api.h>
+#include "hlcoop/api.h"
 
 // engine hook table
 extern enginefuncs_t g_engine_hooks;
@@ -13,6 +14,15 @@ extern DLL_FUNCTIONS g_dll_hooks;
 extern DLL_FUNCTIONS g_dll_hooks_post;
 
 extern NEW_DLL_FUNCTIONS g_newdll_hooks;
+
+extern HLCOOP_FUNCTIONS g_hlcoop_funcs;
+
+extern int g_mod_api; // will be set to the mod ID that an API was loaded for
+
+enum MOD_APIS {
+	MOD_API_NONE = 0, // no mod apis were loaded
+	MOD_API_HLCOOP = 1 // g_hlcoop_funcs was initialized
+};
 
 void PluginInit();
 
