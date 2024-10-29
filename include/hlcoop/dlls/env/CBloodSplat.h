43ceff6f7230988553cdfc9a521e35d182491ca6
wootguy <drake.rhunter@gmail.com>
2024-10-28 19:53:13 -0700

    remove hl co-op build mode
    plugins should be ported to the native build system instead

--- include/hlcoop/dlls/env/CBloodSplat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CBloodSplat : public CBaseEntity
-{
-public:
-	virtual int	GetEntindexPriority() { return ENTIDX_PRIORITY_LOW; }
-	void	Spawn(entvars_t* pevOwner);
-	void	Spray(void);
-};
